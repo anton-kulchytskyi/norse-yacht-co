d10a9f2a6a503d3a2acf850b8883fae813b5f58b
Anton Kulchytskyi <antek1809@gmail.com>
2024-10-18 01:42:55 +0300

    add suspence for modals

--- src/components/MainLayout/NavbarModals.tsx
+++ src/components/MainLayout/NavbarModals.tsx
@@ -1,5 +1,6 @@
 'use client';
 import { useRouter, useSearchParams } from 'next/navigation';
+import { Suspense } from 'react';
 import CurrencyModal from '@/components/Modals/CurrencyModal';
 import MobileMenuModal from '@/components/Modals/MobileMenuModal';
 
@@ -11,10 +12,10 @@ const NavbarModals = () => {
     router.back();
   };
   return (
-    <>
+    <Suspense fallback={<p>loading ...</p>}>
       {modal === 'currency' && <CurrencyModal onClose={closeModal} />}
       {modal === 'mobileMenu' && <MobileMenuModal onClose={closeModal} />}
-    </>
+    </Suspense>
   );
 };
 
