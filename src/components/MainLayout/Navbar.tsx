d892b062ec918f9e8aef0eb966602b6c029f42cc
Anton Kulchytskyi <antek1809@gmail.com>
2024-10-20 16:16:02 +0300

    "Moved nav element inside Suspense component and added fallback"

--- src/components/MainLayout/Navbar.tsx
+++ src/components/MainLayout/Navbar.tsx
@@ -6,10 +6,10 @@ import NavbarModals from './NavbarModals';
 const Navbar = () => {
   return (
     <>
-      <nav className="relative z-20 grid grid-cols-[1fr_auto_1fr] gap-1 items-center pt-4 md:pt-6 xl:pt-8 px-5 md:px-12 xl:px-16">
-        <NavbarLinks />
-      </nav>
       <Suspense fallback={<p>loading ...</p>}>
+        <nav className="relative z-20 grid grid-cols-[1fr_auto_1fr] gap-1 items-center pt-4 md:pt-6 xl:pt-8 px-5 md:px-12 xl:px-16">
+          <NavbarLinks />
+        </nav>
         <NavbarModals />
       </Suspense>
     </>
